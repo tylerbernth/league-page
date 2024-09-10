c64e8c750ac69b405398b9c5814fa1f513303f35
tylerbernth <tylerbernth@gmail.com>
2024-09-09 22:21:59 -0700

    Update leagueInfo.js

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -189,7 +189,7 @@ export const managers = [
       //"tookOver": 2020, // (DEPRECATED! You don't need to use this anymore) (optional) used if a manager took over a team, delete this line or change to null otherwise
       //"location": "Brooklyn", // (optional)
       "bio": "Tyler doesn’t watch movies or Football, but somehow he made a Star Wars reference in a Fantasy Football Keeper league. He has a history of winning, though, so he is living proof that you don’t necessarily need to be all in on Football to win in Fantasy Football. Tyler has lost his edge with the advent of free fantasy football tools and trackers, but his combination of being a data enthusiast and developer gives him multiple avenues to stay in the know.",
-      //"photo": "/managers/name.jpg", // square ratio recommended (no larger than 500x500)
+      "photo": "/managers/tyler.png", // square ratio recommended (no larger than 500x500)
       //"fantasyStart": 2014, // (optional) when did the manager start playing fantasy football
       "favoriteTeam": "ari", // (optional) favorite NFL team, (follows convention: nyj, sea, mia, etc.) MUST BE LOWERCASE
       "mode": "Win Now", // (optional) 'Win Now', 'Dynasty', or 'Rebuild' (anything else and you will need to add a new png to /static/ similar to the 'Rebuild.png' and 'Win Now.png' currently in there)
