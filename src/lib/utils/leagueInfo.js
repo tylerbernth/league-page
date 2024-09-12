2026fe6b9c0450710ce09e699a7dafb15c5ce14d
tylerbernth <tylerbernth@gmail.com>
2024-09-12 13:42:51 -0700

    Update leagueInfo.js

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -22,28 +22,7 @@ https://github.com/nmelhado/league-page/blob/master/TRAINING_WHEELS.md#ii-adding
 // To omit an optional field, set it's value to null
 
 export const managers = [
-   {
-      "managerID": "1133820009303724032",  // the user's manager ID, go to https://api.sleeper.app/v1/league/<your_league_id>/users to find user IDs (you can use older leagueIDs to find user IDs for managers that are no longer in the league)
-      "name": "Tyler Bernth",
-      //"tookOver": 2020, // (DEPRECATED! You don't need to use this anymore) (optional) used if a manager took over a team, delete this line or change to null otherwise
-      //"location": "Brooklyn", // (optional)
-      "bio": "Tyler doesn’t watch movies or Football, but somehow he made a Star Wars reference in a Fantasy Football Keeper league. He has a history of winning, though, so he is living proof that you don’t necessarily need to be all in on Football to win in Fantasy Football. Tyler has lost his edge with the advent of free fantasy football tools and trackers, but his combination of being a data enthusiast and developer gives him multiple avenues to stay in the know.",
-      "photo": "/managers/tyler.png", // square ratio recommended (no larger than 500x500)
-      //"fantasyStart": 2014, // (optional) when did the manager start playing fantasy football
-      "favoriteTeam": "ari", // (optional) favorite NFL team, (follows convention: nyj, sea, mia, etc.) MUST BE LOWERCASE
-      "mode": "Win Now", // (optional) 'Win Now', 'Dynasty', or 'Rebuild' (anything else and you will need to add a new png to /static/ similar to the 'Rebuild.png' and 'Win Now.png' currently in there)
-      //"rival": {
-      //  name: "Rival", // Can be anything (usually your rival's name)
-      //  link: 6, // manager array number within this array, or null to link back to all managers page
-      //  image: "/managers/rival.jpg", // either a specific manager photo or '/managers/everyone.png' or '/managers/question.png'
-      //},
-    //  "favoritePlayer": 1426, // (optional) this corresponds to the Sleeper player ID (https://api.sleeper.app/v1/players/nfl)
-      "valuePosition": "WR", // (optional) Favorite position (QB, WR, RB, TE, etc.)
-      "rookieOrVets": "Vets", // (optional) 'Rookies' or 'Vets' (anything else and you will need to add a new png to /static/ similar to the 'Rookies.png' and 'Vets.png' currently in there)
-      "philosophy": "Your fantasy team's philosophy", // (optional)
-      "tradingScale": 10, // 1 - 10 (optional)
-      "preferredContact": "Text",  // (optional) 'Text', 'WhatsApp', 'Sleeper', 'Email', 'Phone', 'Discord', and 'Carrier Pigeon' are currently supplied in the template
-    },
+
   ]
   
   
