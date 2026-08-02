d6589b49560361c3abc74c30729eb644f3d7a44a
Tyler Bernth <tylerbernth@gmail.com>
2026-08-02 13:50:20 -0700

    Updating new league ID

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -1,5 +1,5 @@
 /*   STEP 1   */
-export const leagueID = "1256429714680053760"; // your league ID
+export const leagueID = "1389372803391692800"; // your league ID
 export const leagueName = "Game of Inches"; // your league name
 export const dues = 50; // (optional) used in template constitution page
 export const dynasty = false; // true for dynasty leagues, false for redraft and keeper
